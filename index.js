e2233428bc084fd98dcb0972bce9821db1e4a054
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-02-14 19:02:06 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(function(game) {
+      return game.result === "W";
+    });
+  
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined;
+    }
+  }
